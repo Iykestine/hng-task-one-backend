55771bbcc85389f0db2f60c21bedb7d00f120d2d
Iykestine <roboticike@yahoo.com>
2023-09-11 05:17:49 +0100

    my first commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace HNGtask.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetProfile(string slack_name="Iykestine", string track="backend")
+        {
+            var originalTime = DateTime.UtcNow;
+
+            var profile = new Profile
+            {
+                Slack_name = slack_name,
+                Current_day = DateTime.Today.DayOfWeek.ToString(),
+                Utc_time = originalTime.ToString("yyyy-MM-ddTHH:mm:ssZ"), // Truncate milliseconds and convert back to string
+                Track = track,
+                Github_file_url = track,
+                Github_repo_url = track,
+                Status_code = 200,
+            };
+
+            return Ok(profile);
+        }
+    }
+}
---
+++ Profile.cs
@@ -0,0 +1,13 @@
+﻿namespace HNGtask
+{
+    public class Profile
+    {
+        public string Slack_name { get; set; }
+        public string Current_day { get; set; }
+        public string Utc_time { get; set; }
+        public string Track { get; set; }
+        public string Github_file_url { get; set; }
+        public string Github_repo_url { get; set; }
+        public int Status_code { get; set; }
+    }
+}
